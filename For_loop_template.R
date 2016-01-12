e990c7b4d5719cd2d7bfd762a0d282e27b751172
kgostic <kategostic@gmail.com>
2016-01-11 22:58:38 -0800

    Update For_loop_template.R
    Made r into a parameter instead of hard code

--- For_loop_template.R
+++ For_loop_template.R
@@ -3,11 +3,14 @@ NN = 1
 #Print out the value of you initial population size
 NN
 
+#Set your population growth rate
+r = 2
+
 #Set the number of time steps
 nsteps = 10
 
 for(ii in 1:nsteps){
-  NN[ii+1] = NN[ii]*2
+  NN[ii+1] = NN[ii]*r
   print(NN)
 }
 
